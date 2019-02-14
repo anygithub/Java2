a16cff3e97af874be2b118cbf6e0e93d481ab044
Anna Bondarenko <postportal@mail.ru>
2019-02-14 23:42:55 +0300

    ArrayHandler class is added

---
+++ Homework2/src/ArrayHandler.java
@@ -0,0 +1,20 @@
+import excepthandler.MyArraySizeException;
+
+/**
+ * @author Anna Bondarenko
+ * @version 14.02.19
+ */
+public class ArrayHandler {
+
+    public boolean CheckArray(int[][] array) {
+        try {
+
+        } catch (MyArraySizeException mase) {
+            
+        }
+
+        return false;
+    }
+
+
+}
