3264b58c9d9756bff39cc99fddddac0df79dbe55
Anna Bondarenko <postportal@mail.ru>
2019-02-19 17:01:31 +0300

    final version of ListHandler class

--- Homework3/src/ListHandler.java
+++ Homework3/src/ListHandler.java
@@ -2,13 +2,15 @@
  * @author Anna Bondarenko
  * @version 19.02.19
  */
-import java.util.ArrayList;
-import java.util.List;
+import java.util.*;
 
 
 public class ListHandler {
 
-
+    /**
+     * Generate arrayList
+     * @param arrayList
+     */
     public void generateList(List<String> arrayList) {
         arrayList.add("apple");
         arrayList.add("pineapple");
@@ -25,6 +27,25 @@ public class ListHandler {
         arrayList.add("melon");
         arrayList.add("melon");
         arrayList.add("pear");
+    }
+
+    /**
+     * Get list of unique words from the list
+     * @param list
+     * @return
+     */
+    public Set<String> getListOfUniqueWords(List<String> list) {
+        Set<String> set = new HashSet<>();
+        for (String s:list) {
+           set.add(s);
+        }
+        return set;
+    }
+
+    public void printCountOfUniqueWords(List<String> list, Set<String> set) {
+        for (String s:set) {
+            System.out.println(s + "  " + Collections.frequency(list, s));
+        }
     }
 
 
