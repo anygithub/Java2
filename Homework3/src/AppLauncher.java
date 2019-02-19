4cc05d54d96e62924b8a1241d687b07649c38cc6
Anna Bondarenko <postportal@mail.ru>
2019-02-19 17:54:25 +0300

    code for exercise 2 added

--- Homework3/src/AppLauncher.java
+++ Homework3/src/AppLauncher.java
@@ -1,23 +1,23 @@
-import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
-
 /**
  * @author Anna Bondarenko
  * @version 19.02.19
  */
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Set;
 
 public class AppLauncher {
-    
+
     public static void main(String[] args) {
+        System.out.println("Exercise1");
         runExercise1();
+        System.out.println();
+
+        System.out.println("Exercise2");
+        runExercise2();
     }
 
-    /* Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
-       Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
-       Посчитать сколько раз встречается каждое слово. */
+    //Exercise1
     public static void runExercise1() {
         List<String> arrayList = new ArrayList<>();
         ListHandler listHandler = new ListHandler();
@@ -35,6 +35,22 @@ public class AppLauncher {
         listHandler.printCountOfUniqueWords(arrayList,hashSet);
     }
 
+    //Exercise2
+    public static void runExercise2() {
+        List<PhoneBookRecord> phoneBook = new ArrayList<>();
+        PhoneBookHandler phoneBookHandler = new PhoneBookHandler();
+        String numbers = new String();
 
+        // generate PhoneBook
+        phoneBookHandler.generatePhoneBook(phoneBook);
 
+        // add new contact in PhoneBook
+        phoneBook.add(new PhoneBookRecord("Lermontov","10101010"));
+        System.out.println(phoneBook);
+
+        // get phone numbers by surname
+        numbers = phoneBookHandler.get(phoneBook,"Lermontov");
+        System.out.println(numbers);
+
+    }
 }
