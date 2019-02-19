58974a6038a519072ad2e5345c59e53ddcf97d46
Anna Bondarenko <postportal@mail.ru>
2019-02-19 11:05:51 +0300

    ListHandler class added

---
+++ Homework3/src/ListHandler.java
@@ -0,0 +1,31 @@
+/**
+ * @author Anna Bondarenko
+ * @version 19.02.19
+ */
+import java.util.ArrayList;
+import java.util.List;
+
+
+public class ListHandler {
+
+
+    public void generateList(List<String> arrayList) {
+        arrayList.add("apple");
+        arrayList.add("pineapple");
+        arrayList.add("banana");
+        arrayList.add("grape");
+        arrayList.add("plum");
+        arrayList.add("melon");
+        arrayList.add("strawberry");
+        arrayList.add("apple");
+        arrayList.add("banana");
+        arrayList.add("grape");
+        arrayList.add("grape");
+        arrayList.add("pineapple");
+        arrayList.add("melon");
+        arrayList.add("melon");
+        arrayList.add("pear");
+    }
+
+
+}
