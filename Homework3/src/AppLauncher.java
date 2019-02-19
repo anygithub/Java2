87e8985019af524c85ce903ba45caa30fb224d6f
Anna Bondarenko <postportal@mail.ru>
2019-02-19 17:02:30 +0300

    runExercise1 method added

--- Homework3/src/AppLauncher.java
+++ Homework3/src/AppLauncher.java
@@ -1,5 +1,7 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Set;
 
 /**
  * @author Anna Bondarenko
@@ -12,18 +14,29 @@ public class AppLauncher {
 
 
     public static void main(String[] args) {
-        //generate list of strings
-        List<String> arrayList = new ArrayList<>();
-        ListHandler listHandler = new ListHandler();
-
-        listHandler.generateList(arrayList);
-        System.out.println(arrayList.toString());
-
-        //sort the list
-        //count
-
+        runExercise1();
     }
 
+    /* Создать массив с набором слов (10-20 слов, должны встречаться повторяющиеся).
+       Найти и вывести список уникальных слов, из которых состоит массив (дубликаты не считаем).
+       Посчитать сколько раз встречается каждое слово. */
+
+    public static void runExercise1() {
+        List<String> arrayList = new ArrayList<>();
+        ListHandler listHandler = new ListHandler();
+        Set<String> hashSet;//
+
+        //generate list of strings
+        listHandler.generateList(arrayList);
+        System.out.println(arrayList);
+
+        //get the list of unique words
+        hashSet = listHandler.getListOfUniqueWords(arrayList);
+        System.out.println(hashSet);
+
+        // get count for each unique word
+        listHandler.printCountOfUniqueWords(arrayList,hashSet);
+    }
 
 
 
